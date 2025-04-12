65834cf8621c6af01b482c66f0e132b5b30cac39
Randy <ciprianrandy@gmail.com>
2025-04-12 13:55:46 -0400

    optimizacion

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
 
-
-//routing
 app.get('/',(req,res)=>{
-    res.send('hola')
+    res.send('hola maria')
 })
 
 
-
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 app.listen(port,()=>{
-    console.log(`servidor iniciado en el puerto ${port}`)
+    console.log('servidor funcionando en el port: ',port)
 })
